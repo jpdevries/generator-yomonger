8f86052fa5c0d9cec7abf1dd08a3deef2870f46c
JP DeVries <johnpaul.devries@gmail.com>
2013-11-22 18:41:44 -0800

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,184 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var _s = require('underscore.string');
+
+var YomodageGenerator = module.exports = function YomodageGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  /*this.on('end', function () {
+    this.installDependencies({
+	  skipInstall: options['skip-install'],
+	  callback: function() {
+		this.emit('dependenciesInstalled');
+	  }.bind(this)
+    });
+  });*/
+
+  this.on('dependenciesInstalled', function() {
+      this.spawnCommand('grunt', ['build']);
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(YomodageGenerator, yeoman.generators.Base);
+
+YomodageGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [
+  {
+	name : 'name',
+	message : 'What would you like to name your template?',
+	default : 'modxtheme'
+  },
+  {
+    name : 'description',
+	message : 'How would you describe this manager theme?',
+    default : 'The best MODX theme ever made!'
+  },
+  {
+    type : 'confirm',
+    name : 'useTpl',
+    message : 'Would you like to start of with the default .tpl and image files?'
+},
+  {
+	type : 'list',
+    name : 'cssType',
+    message : 'Choose a CSS pre-prepocessor',
+    choices : ['Sass','LESS','None'],
+    default: 'Sass'
+  },
+  {
+	type:'confirm',
+    name : 'modxSass',
+	message : 'Since you chose Sass, would you like to start include the source Sass files of the default theme?',
+    default : true,
+    when : function(props) {
+      return props.cssType == 'Sass';
+    }
+  },
+  {
+    name:'templateDir',
+    message : 'What is the relative path to your template directory?',
+    default:function(props) {
+      return '../../../manager/templates/';
+    }
+  },
+  {
+    type:'confirm',
+    name:'enterAuthor',
+    message:'Would you like to enter some author and project related info?',
+    default:true
+},
+  {
+	name : 'authorName',
+	message : 'Author name',
+	default : '',
+    when : function(props) {
+      return props.enterAuthor;
+    }
+  },
+  {
+	name : 'authorEmail',
+	message : 'Author email',
+	default : '',
+    when : function(props) {
+      return props.enterAuthor;
+    }
+  },
+  {
+	name : 'authorUrl',
+	message : 'Author url',
+	default : '',
+    when : function(props) {
+      return props.enterAuthor;
+    }
+  },
+  {
+	name : 'gitRepo',
+	message : 'Project git repository',
+	default : function(props) {
+		return (props.enterAuthor && props.authorName) ? 'git://github.com/' + _s.slugify(props.authorName.replace(/ /g,'')) + '/' + _s.slugify(props.name) + '.git' : ''
+	},
+    when : function(props) {
+      return props.enterAuthor;
+    }
+  },
+  {
+	name : 'gitIssues',
+	message : 'Project issues tracker',
+	default : function(props) {
+		if(!props.gitRepo) return '';
+		var _s = props.gitRepo;
+		_s = _s.substring(0,_s.length-4);
+		return _s.replace('git://','https://') + '/issues';
+	},
+    when : function(props) {
+      return props.gitRepo && props.gitRepo.length > 0;
+    }
+  }
+];
+
+  this.prompt(prompts, function (props) {
+    this.name = props.name;
+	this.prefix = 'modx-';
+    this.description = props.description;
+    this.modxSass = props.modxSass;
+    this.useTpl = props.useTpl;
+	this.slug = _s.slugify(props.name);
+    this.templateDir = (props.templateDir.slice(-1) == '/') ? props.templateDir : props.templateDir + '/';
+    this.themeDir = this.templateDir + this.slug;
+    this.authorName = props.authorName;
+    this.authorEmail = props.authorEmail;
+    this.authorUrl = props.authorUrl;
+    this.gitRepo = props.gitRepo;
+    this.gitIssues = props.gitIssues;
+
+	switch(props.cssType.toLowerCase()[0]) {
+		case 's':
+		default:
+		this.cssType = 'sass';
+		break;
+		
+		case 'l':
+		this.cssType = 'less';
+		break;
+		
+		case 'n':
+		case undefined:
+		this.cssType = 'none';
+		break;
+	}
+	
+    cb();
+  }.bind(this));
+};
+
+YomodageGenerator.prototype.app = function app() {
+  this.template('_package.json', 'package.json');
+  this.template('_bower.json', 'bower.json');
+  this.template('_Gruntfile.js', 'Gruntfile.js');
+  if(this.modxSass) {
+	this.directory('sass','sass');
+
+  } else if(this.cssType == 'sass' || this.cssType == 'less') {
+	// just blank files either way
+    this.mkdir(this.cssType);
+	this.write(this.cssType + '/index.' + this.cssType,'');
+	this.write(this.cssType + '/login.' + this.cssType,'');
+  } 
+  if(this.useTpl) {
+    this.bulkDirectory('default',this.templateDir + this.slug);
+  }
+};
+
+YomodageGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
---
+++ app/templates/_Gruntfile.js
@@ -0,0 +1,230 @@
+module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		dirs: { /* just defining some properties */
+			lib: './lib/',
+			<% if ('sass' === cssType) { %>scss: './sass/',<% } else if ('less' === cssType) {  %>less: './less/',<% } %>
+			template: '<%= themeDir %>',
+			css: '<%%= dirs.template %>css/',
+		},
+		bower: {
+			install: {
+				options: {
+					targetDir: './lib',
+					layout:'byComponent'
+				}
+			}
+		},
+		copy: { /* move files */
+			<% if (modxSass) { %>
+			bourbon: {
+				files:[
+					{src:'bourbon/**/*',cwd:'<%%= dirs.lib %>',dest:'<%%= dirs.scss %>',expand:true}
+				]
+			},
+			fontawesome: {
+				files:[
+					{src: '<%%= dirs.lib %>font-awesome/scss/**/*.scss',dest:'<%%= dirs.scss %>font-awesome/',expand:true,flatten:true},
+					{src: 'font/**/*',cwd:'<%%= dirs.lib %>font-awesome/',dest:'<%%= dirs.template %>',expand:true}
+				]
+			}
+			<% } %>
+		},
+		cssmin: {
+			compress: {
+				options: {
+					report: 'min',
+					keepSpecialComments:1,
+					//banner: '/*!\n* <%%= pkg.title %> - v<%%= pkg.version %> - <%%= grunt.template.today("yyyy-mm-dd") %> \n* see https://github.com/modxcms/revolution/tree/develop/_build/templates/default\n*/'
+					banner : '/*!'
++  '\n* '
++  '\n* Copyright (C) <%%= grunt.template.today("yyyy") %> <%= prefix %>'
++  '\n* '
++  '\n* This file is part of <%%= pkg.title %> and was compiled using Grunt.'
++  '\n* '
++  '\n* <%%= pkg.title %> is free software: you can redistribute it and/or modify it under the terms of the'
++  '\n* GNU General Public License as published by the Free Software Foundation, either version 2 of the'
++  '\n* License, or (at your option) any later version.'
++  '\n* '
++  '\n* <%%= pkg.title %> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;'
++  '\n* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.'
++  '\n* '
++  '\n* See the GNU General Public License for more details. You should have received a copy of the GNU'
++  '\n* General Public License along with <%%= pkg.title %>. If not, see <http://www.gnu.org/licenses/>.'
++  '\n* '
+//+  '\n* Authors: TODO'
++  '\n*/'
+				},
+				files: {
+					'<%%= dirs.css %>index.min.css': '<%%= dirs.css %>index.css',
+					'<%%= dirs.css %>login.min.css': '<%%= dirs.css %>login.css'
+				}
+			},
+			ship: {
+				options: {
+					report: 'min',
+					keepSpecialComments:1,
+					//banner: '/*!\n* <%%= pkg.title %> - v<%%= pkg.version %> - <%%= grunt.template.today("yyyy-mm-dd") %> \n* see https://github.com/modxcms/revolution/tree/develop/_build/templates/default\n*/'
+					banner : '/*!'
++  '\n* <%%= pkg.title %> - v<%%= pkg.version %> - <%%= grunt.template.today("yyyy-mm-dd") %>'
++  '\n* '
++  '\n* Copyright (C) <%%= grunt.template.today("yyyy") %> <%= prefix %>'
++  '\n* '
++  '\n* This file is part of <%%= pkg.title %> and was compiled using Grunt.'
++  '\n* '
++  '\n* <%%= pkg.title %> is free software: you can redistribute it and/or modify it under the terms of the'
++  '\n* GNU General Public License as published by the Free Software Foundation, either version 2 of the'
++  '\n* License, or (at your option) any later version.'
++  '\n* '
++  '\n* <%%= pkg.title %> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;'
++  '\n* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.'
++  '\n* '
++  '\n* See the GNU General Public License for more details. You should have received a copy of the GNU'
++  '\n* General Public License along with <%%= pkg.title %>. If not, see <http://www.gnu.org/licenses/>.'
++  '\n* '
+//+  '\n* Authors: TODO'
++  '\n*/'
+				},
+				files: {
+					'<%%= dirs.css %>index.min.css': '<%%= dirs.css %>index.css',
+					'<%%= dirs.css %>login.min.css': '<%%= dirs.css %>login.css'
+				}
+			}
+		},
+		<% if ('sass' === cssType) { %>
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+					compass: false
+				},
+				files: {
+					'<%%= dirs.css %>index.css': 'sass/index.scss',
+					'<%%= dirs.css %>login.css': 'sass/login.scss'
+				}
+			},
+			dev: {
+				options: {
+					style: 'expanded',
+					compass: false
+				},
+				files: {
+					'<%%= dirs.css %>index.css': 'sass/index.scss',
+					'<%%= dirs.css %>login.css': 'sass/login.scss'
+				}
+			}
+		},
+		<% } else if ('less' === cssType) {  %>
+		less : {
+			dist : {
+				files: {
+					'<%%= dirs.css %>index.css': 'less/index.less',
+					'<%%= dirs.css %>login.css': 'less/login.less'
+				}
+			},
+			dev : {
+				compress: false,
+				files: {
+					'<%%= dirs.css %>index.css': 'less/index.less',
+					'<%%= dirs.css %>login.css': 'less/login.less'
+				}
+			}
+		},
+		<% } %>
+		autoprefixer: { /* this expands the css so it needs to get compressed with cssmin afterwards */
+			options: {
+				// Task-specific options go here.
+			},
+
+			// just prefix the specified file
+			index: {
+				options: {},
+				src: '<%%= dirs.css %>index.min.css',
+				dest: '<%%= dirs.css %>index.min.css'
+			},
+			login: {
+				options: {},
+				src: '<%%= dirs.css %>login.min.css',
+				dest: '<%%= dirs.css %>login.min.css'
+			}
+		},
+		csslint: {
+		  strict: {
+		    options: {
+		      import: 2
+		    },
+		    src: ['<%%= dirs.css %>*.css']
+		  }
+		},
+		watch: { /* trigger tasks on save */
+			options: {
+				livereload: true
+			},
+			<% if ('sass' === cssType) { %>
+			scss: {
+				files: ['<%%= dirs.scss %>*','<%%= dirs.scss %>components/**/*'],
+				tasks: ['sass:dist', 'autoprefixer', 'cssmin:compress', 'growl:sass']
+			}
+			<% } else if ('less' === cssType) {  %>
+			less: {
+				files: ['<%%= dirs.less %>*'],
+				tasks: ['less:dist','autoprefixer','cssmin:compress','growl:less']
+			}
+			<% } %>
+		},
+		clean: { /* take out the trash */
+			<% if (modxSass) { %>prebuild: ['<%%= dirs.scss %>bourbon','<%%= dirs.scss %>font-awesome'],<% } %>
+			postbuild: ['<%%= dirs.lib %>']
+		},
+		growl: {
+			<% if ('sass' === cssType) { %>
+			sass: {
+				message: "Sass files created.",
+				title: "grunt"
+			},
+			<% } else if ('less' === cssType) {  %>
+			less: {
+				message: "Less files created.",
+				title: "grunt"
+			},
+			<% } %>
+			build: {
+				title: "grunt",
+				message: "Build complete."
+			},
+			prefixes: {
+				title: "grunt",
+				message: "CSS prefixes added."
+			},
+			watch: {
+				title: "grunt",
+				message: "Watching. Grunt has its eye on you."
+			},
+			expand: {
+				title: "grunt",
+				message: "CSS Expanded. Don't check it in."
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	<% if ('sass' === cssType) { %>
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	<% } else if ('less' === cssType) {  %>
+	grunt.loadNpmTasks('grunt-contrib-less');
+	<% } %>
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-growl');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+
+	// Tasks
+	grunt.registerTask('default', ['sass:dist', 'autoprefixer', 'growl:prefixes', 'growl:sass', 'cssmin:compress', 'growl:watch', 'watch']);
+	grunt.registerTask('build', ['clean:prebuild','bower', 'copy', <% if ('sass' === cssType) { %>'sass:dist', <% } else if ('less' === cssType) {  %>'less:dist', <% } %>'autoprefixer', 'growl:prefixes', <% if ('sass' === cssType) { %>'growl:sass', <% } else if ('less' === cssType) {  %>'growl:less', <% } %>'cssmin:compress','clean:postbuild']);
+	grunt.registerTask('expand', [<% if ('sass' === cssType) { %>'sass:dev', <% } else if ('less' === cssType) {  %>'less:dev', <% } %> 'autoprefixer', 'growl:prefixes', 'growl:sass', 'growl:expand']);
+	grunt.registerTask('ship', ['clean:prebuild','bower', 'copy', <% if ('sass' === cssType) { %>'sass:dist', <% } else if ('less' === cssType) {  %>'less:dist', <% } %>,'autoprefixer', 'growl:prefixes', <% if ('sass' === cssType) { %>'growl:sass', <% } else if ('less' === cssType) {  %>'growl:less', <% } %>'cssmin:ship','clean:postbuild']);
+};
